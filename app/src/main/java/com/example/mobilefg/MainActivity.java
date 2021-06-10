22a490740431a7070b0c6ef982d8923e64235e95
Omer eckstein <72710470+omereks@users.noreply.github.com>
2021-06-10 10:48:32 +0300

    First Install

---
+++ app/src/main/java/com/example/mobilefg/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.mobilefg;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Model model = new Model("", 0);
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = (TextView)findViewById(R.id.textViewTitle);
+
+        setContentView(R.layout.activity_main);
+
+        EditText editTextIP = (EditText)findViewById(R.id.editTextIP);
+        EditText editTextPort = (EditText)findViewById(R.id.editTextPort);
+        Button buttonConnect = (Button)findViewById(R.id.buttonConnect);
+
+        SeekBar seekBarThrottle = (SeekBar)findViewById(R.id.seekBarThrottle);
+        seekBarThrottle.setMin(0);
+        seekBarThrottle.setMax(10);
+
+        SeekBar seekBarRudder = (SeekBar)findViewById(R.id.seekBarRudder);
+        seekBarRudder.setMin(-10);
+        seekBarRudder.setMax(10);
+
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                model = new Model(editTextIP.getText().toString(), Integer.parseInt(editTextPort.getText().toString()));
+                model.Connect();
+
+
+            }
+        });
+
+        seekBarThrottle.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                model.setThrottle((float)progress / 10.0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        seekBarRudder.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                model.setRudder((float)progress / 10.0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/mobilefg/Model.java
@@ -0,0 +1,28 @@
+package com.example.mobilefg;
+
+public class Model {
+    int port;
+    String IP;
+    double throttle;
+    double rudder;
+
+    public void setThrottle(double throttle) {
+        this.throttle = throttle;
+    }
+
+    public void setRudder(double rudder) {
+        this.rudder = rudder;
+    }
+
+    Model(String IP, int port){
+        this.IP = IP;
+        this.port = port;
+        this.throttle = 0;
+        this.rudder = 0;
+    }
+
+    void Connect(){
+        // kvar
+    }
+
+}
